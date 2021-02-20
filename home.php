393866f9cd9b1b07bcb1705bdafd7032714ad920
2ndyrGroupB <2ndyrGroupB@pn.local>
2021-02-20 21:05:33 +0800

    aiai20

--- home.php
+++ home.php
@@ -30,17 +30,22 @@ $result = mysqli_fetch_assoc($data);
 ?>
 
 <!DOCTYPE html>
+<link rel="stylesheet" href="homestyle.css">
+
 <script src="jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <html lang="en">
 <head>
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  
 </head>
-<body>
+<body >
+
 
 <!-- CONNECTING THE DATABASE -->
 <?php require_once 'connection.php';?>
--- login.php
+++ login.php
@@ -6,12 +6,71 @@ include("connection.php");
 
 
 
-<form action = "" method = "post">
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
 
-Email: <input type = "email" name="email" value=""/><br><br>
-Password: <input type = "password" name="password" value=""/><br><br>
-<input type = "submit" name="submit" value="Login"/>
-</form>
+<body>
+
+
+    <script src="js/prefixfree.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+
+
+    </head>
+
+    <body>
+
+        <div id="logo">
+            <h1><i>ZIGM CRUD</i></h1>
+        </div>
+        <section class="stark-login">
+
+           <form action = "" method = "post">
+                <div id="fade-box">
+                    <input type="email" name="email" id="username" placeholder="Email" required>
+                    <input type="password" placeholder="Password" name="password" required>
+                    <input type = "submit" name="submit" value="Login"/>
+
+                    
+                </div>
+            </form>
+            <div class="hexagons">
+
+                <img src="https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&w=1000&q=80" height="768px" width="1366px" />
+            </div>
+        </section>
+
+        <div id="circle1">
+            <div id="inner-cirlce1">
+                <h2> </h2>
+            </div>
+        </div>
+
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+        <script src='https://codepen.io/assets/libs/fullpage/jquery.js'></script>
+
+        <script src="js/index.js"></script>
+
+    </body>
+
+</html>
+</body>
+
+</html>
+
+
+
+
+
+
 
 
 
